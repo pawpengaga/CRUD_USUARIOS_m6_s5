be69fc0a04f9c492d4e097c0f713e30cb6c760f3
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-06 19:25:04 -0300

    usuario creado

--- src/main/java/com/pawpengaga/modelo/Usuario.java
+++ src/main/java/com/pawpengaga/modelo/Usuario.java
@@ -1,10 +1,18 @@
 package com.pawpengaga.modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Usuario {
 
   private String rut;
   private String nombre;
   private String clave;
   private String correo;
+  private String usuario;
 
 }
