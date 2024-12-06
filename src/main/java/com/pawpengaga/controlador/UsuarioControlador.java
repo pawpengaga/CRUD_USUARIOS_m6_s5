73345cdfba25f8e9915483c646fbb2d4685370ce
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-06 19:47:44 -0300

    controlador listo por ahora

--- src/main/java/com/pawpengaga/controlador/UsuarioControlador.java
+++ src/main/java/com/pawpengaga/controlador/UsuarioControlador.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,7 +20,9 @@ public class UsuarioControlador {
   @Autowired
   IUsuario userService;
 
-  @GetMapping("/inicio")
+  String respuesta;
+
+  @GetMapping({"/", "/inicio"})
   public ModelAndView inicio(){
     ModelAndView mav = new ModelAndView("inicio");
     List<Usuario> listado = userService.listaUsuarios();
@@ -26,5 +30,15 @@ public class UsuarioControlador {
     return mav;
   }
 
+  @GetMapping("/formulario")
+  public String formulario(){
+    return "formulario";
+  }
+
+  @PostMapping("/grabar")
+  public String grabarUsuario(@ModelAttribute("Usuario") Usuario user){
+    respuesta = userService.agregaUsuario(user);
+    return "redirect:/inicio";
+  }
 
 }
